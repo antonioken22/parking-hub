2a634407b89a8a203c3b6a3456750f1678a4f4bd
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-07-02 01:58:00 +0800

    [Feature][Antonio] Added parking slot editable information

--- app/(routes)/parking-map/rtl-open-court/_components/parking-layout.tsx
+++ app/(routes)/parking-map/rtl-open-court/_components/parking-layout.tsx
@@ -7,23 +7,30 @@ import {
   doc,
   updateDoc,
   addDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 
 import { auth, firestore } from "@/firebase/config";
-import ParkingSlotComponent from "./parking-slot";
+import ParkingSlot from "./parking-slot";
 import { Button } from "@/components/ui/button";
+import RTLOpenCourtImage from "@/public/rtl-open-court-zoomed.png";
 
-interface ParkingSlot {
+interface ParkingSlotData {
   id: string;
   top: number;
   left: number;
   color: string;
+  status: string;
+  name: string | null;
+  countdown: Date | null;
+  description: string | null;
+  pushNotification: boolean;
 }
 
 const ParkingLayout = () => {
   const [userRole, setUserRole] = useState<string | null>(null);
-  const [parkingSlots, setParkingSlots] = useState<ParkingSlot[]>([]);
+  const [parkingSlots, setParkingSlots] = useState<ParkingSlotData[]>([]);
   const [selectedSlotIndex, setSelectedSlotIndex] = useState<number | null>(
     null
   );
@@ -48,8 +55,8 @@ const ParkingLayout = () => {
         const slotsRef = collection(firestore, "RTLOpenCourtParkingSlots");
         const slotsSnapshot = await getDocs(slotsRef);
 
-        const slotsData: ParkingSlot[] = slotsSnapshot.docs.map(
-          (doc) => ({ id: doc.id, ...doc.data() } as ParkingSlot)
+        const slotsData: ParkingSlotData[] = slotsSnapshot.docs.map(
+          (doc) => ({ id: doc.id, ...doc.data() } as ParkingSlotData)
         );
 
         setParkingSlots(slotsData);
@@ -77,17 +84,60 @@ const ParkingLayout = () => {
     setParkingSlots(updatedSlots);
   };
 
+  const handleEditSlot = (index: number, updatedSlot: ParkingSlotData) => {
+    const updatedSlots = [...parkingSlots];
+    updatedSlots[index] = { ...updatedSlot };
+    setParkingSlots(updatedSlots);
+    // console.log(updatedSlot);
+  };
+
+  // Add in Firestore
+  const addParkingSlot = async () => {
+    try {
+      // Center spawn
+      // Default status
+      const newSlot = {
+        top: 50,
+        left: 50,
+        color: "green",
+        status: "Unoccupied",
+        name: null,
+        countdown: null,
+        description: null,
+        pushNotification: false,
+      };
+
+      const slotsRef = collection(firestore, "RTLOpenCourtParkingSlots");
+      const docRef = await addDoc(slotsRef, newSlot);
+      const updatedSlots = [...parkingSlots, { id: docRef.id, ...newSlot }];
+
+      setParkingSlots(updatedSlots);
+
+      console.log("New parking slot added successfully!");
+    } catch (error) {
+      console.error("Error adding new parking slot:", error);
+    }
+  };
+
+  // Update in Firestore
   const saveParkingSlots = async () => {
     try {
       const slotsRef = collection(firestore, "RTLOpenCourtParkingSlots");
 
-      // Update in Firestore
       await Promise.all(
         parkingSlots.map(async (slot) => {
+          if (!slot.id) {
+            throw new Error("Missing document ID for one or more slots.");
+          }
           await updateDoc(doc(slotsRef, slot.id), {
             top: slot.top,
             left: slot.left,
             color: slot.color,
+            status: slot.status || null,
+            name: slot.name || null,
+            countdown: slot.countdown || null,
+            description: slot.description || null,
+            pushNotification: slot.pushNotification || null,
           });
         })
       );
@@ -98,23 +148,32 @@ const ParkingLayout = () => {
     }
   };
 
-  const addParkingSlot = async () => {
+  // Delete in Firestore
+  const deleteParkingSlot = async () => {
+    if (selectedSlotIndex === null) {
+      console.warn("No slot selected for deletion");
+      return;
+    }
+
     try {
-      // Center spawn
-      const newSlot = {
-        top: 50,
-        left: 50,
-        color: "gray",
-      };
+      const slotToDelete = parkingSlots[selectedSlotIndex];
+      const slotRef = doc(
+        firestore,
+        "RTLOpenCourtParkingSlots",
+        slotToDelete.id
+      );
 
-      const slotsRef = collection(firestore, "RTLOpenCourtParkingSlots");
-      const docRef = await addDoc(slotsRef, newSlot);
+      await deleteDoc(slotRef);
 
-      const updatedSlots = [...parkingSlots, { id: docRef.id, ...newSlot }];
-
+      const updatedSlots = parkingSlots.filter(
+        (_, index) => index !== selectedSlotIndex
+      );
       setParkingSlots(updatedSlots);
+      setSelectedSlotIndex(null);
+
+      console.log("Parking slot deleted successfully!");
     } catch (error) {
-      console.error("Error adding parking slot:", error);
+      console.error("Error deleting parking slot:", error);
     }
   };
 
@@ -122,31 +181,45 @@ const ParkingLayout = () => {
     <div className="pt-4 flex justify-center items-center">
       <div className="relative">
         {parkingSlots.map((slot, index) => (
-          <ParkingSlotComponent
+          <ParkingSlot
             key={slot.id}
             slot={slot}
             index={index}
             onPositionChange={handleSlotPositionChange}
             onColorChange={handleColorChange}
+            onEdit={handleEditSlot}
             role={userRole}
             selected={index === selectedSlotIndex}
             onSelect={() => setSelectedSlotIndex(index)}
           />
         ))}
         <Image
-          loading="lazy"
-          alt="CIT-U Vicinity Map"
-          src="/rtl-open-court-zoomed.png"
+          alt="RTL Open Court"
+          src={RTLOpenCourtImage}
           layout="intrinsic"
           width={1448 / 2.6}
           height={2048 / 2.6}
+          placeholder="blur"
+          priority
         />
         {userRole === "admin" && (
-          <div className="absolute bottom-4 right-4 space-x-2">
-            <Button className="shadow-md " onClick={addParkingSlot}>
+          <div className="absolute bottom-2 right-2 md:bottom-4 md:right-4 space-x-1 md:space-x-2">
+            <Button
+              className="text-xs md:text-base shadow-md "
+              onClick={deleteParkingSlot}
+            >
+              Delete Slot
+            </Button>
+            <Button
+              className="text-xs md:text-base shadow-md "
+              onClick={addParkingSlot}
+            >
               Add Slot
             </Button>
-            <Button className="shadow-md " onClick={saveParkingSlots}>
+            <Button
+              className="text-xs md:text-base shadow-md "
+              onClick={saveParkingSlots}
+            >
               Save Changes
             </Button>
           </div>
--- app/(routes)/parking-map/rtl-open-court/page.tsx
+++ app/(routes)/parking-map/rtl-open-court/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
 import { SquareParking, Undo2 } from "lucide-react";
 
-import { auth, firestore } from "@/firebase/config";
 import { Heading } from "@/app/(routes)/_components/heading";
 import ParkingLayout from "./_components/parking-layout";
 
 const RTLOpenCourtPage = () => {
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const userDoc = await getDoc(doc(firestore, "users", user.uid));
-        if (userDoc.exists()) {
-        }
-      }
-    });
-
-    // Cleanup subscription on unmount
-    return () => unsubscribe();
-  }, []);
-
   return (
     <div>
       <div className="flex items-center gap-x-3 mr-auto pl-4">
