9e03049231a32dcb5640599f62e9beebc2e585b0
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-07-03 16:46:49 +0800

    [Adjustment][Antonio] Improved selected parking slot drawer UI

--- app/(routes)/parking-map/rtl-open-court/_components/parking-slot-info-drawer.tsx
+++ app/(routes)/parking-map/rtl-open-court/_components/parking-slot-info-drawer.tsx
@@ -48,48 +48,51 @@ export function ParkingSlotInfoDrawer({
     <Drawer open={isOpen} onOpenChange={handleOpenChange}>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm">
-          <DrawerHeader className="flex justify-between items-center">
-            <div className="flex items-center flex-grow">
-              <div className="flex items-center">
-                <Image
-                  alt="Logo"
-                  src={Logo}
-                  className="w-6 h-6 md:w-10 md:h-10 mr-2 text-primary dark:hidden"
-                  priority
-                />
-                <Image
-                  alt="Logo"
-                  src={LogoDark}
-                  className="w-6 h-6 md:w-10 md:h-10 mr-2 text-primary hidden dark:block"
-                  priority
-                />
-              </div>
-              <div className="flex flex-col">
-                <DrawerTitle className="mb-1">Parking Slot Info</DrawerTitle>
-                <DrawerDescription className="text-xs md:text-sm "></DrawerDescription>
-              </div>
+          <DrawerHeader className="relative flex items-center">
+            <div className="flex items-center">
+              <Image
+                alt="Logo"
+                src={Logo}
+                className="w-10 h-10 mr-2 text-primary dark:hidden border border-primary rounded"
+                priority
+              />
+              <Image
+                alt="Logo"
+                src={LogoDark}
+                className="w-10 h-10 mr-2 p-1 text-primary hidden dark:block border border-primary rounded"
+                priority
+              />
+            </div>
+            <div className="flex flex-col flex-grow">
+              <DrawerTitle className="mb-1 mr-auto text-base md:text-xl">
+                Parking Slot Info
+              </DrawerTitle>
+              <DrawerDescription className="text-xs md:text-sm mr-auto text-wrap">
+                Viewing selected parking slot status.
+              </DrawerDescription>
             </div>
             <div
-              className="w-6 h-6 md:w-8 md:h-8 rounded-full"
+              className="absolute top-0 right-4 w-6 h-6 md:w-8 md:h-8 rounded-full"
               style={{ backgroundColor: slot.color }}
               title={slot.status}
             ></div>
           </DrawerHeader>
+
           <div className="p-4">
-            <div className="mb-4">
+            <div className="text-sm md:text-base mb-4">
               <strong>Status:</strong> {slot.status}
             </div>
-            <div className="mb-4">
+            <div className="text-sm md:text-base mb-4">
               <strong>Name:</strong> {slot.name || "N/A"}
             </div>
-            <div className="mb-4">
+            <div className="text-sm md:text-base mb-4">
               <strong>Description:</strong> {slot.description || "N/A"}
             </div>
-            <div className="mb-4">
+            <div className="text-sm md:text-base mb-4">
               <strong>Start Time:</strong>{" "}
               {slot.startTime ? formatDateTime(slot.startTime) : "N/A"}
             </div>
-            <div className="mb-4">
+            <div className="text-sm md:text-base mb-4">
               <strong>End Time:</strong>{" "}
               {slot.endTime ? formatDateTime(slot.endTime) : "N/A"}
             </div>
