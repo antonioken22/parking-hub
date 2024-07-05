67dea8092d464f2ed4407015d99f6bff9a3281b7
GiriGiriAiWo <ryangwapo321321321@gmail.com>
2024-07-05 10:30:50 +0800

    fixed extra rows and added mobile responsiveness

--- app/(routes)/dashboard/components/dashboard-data-list-logs.tsx
+++ app/(routes)/dashboard/components/dashboard-data-list-logs.tsx
@@ -1,31 +1,39 @@
-import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState, useCallback } from 'react';
+import { collection, getDocs, Timestamp } from 'firebase/firestore';
 import { firestore } from "@/firebase/config";
 
 type User = {
   id: string;
   firstName: string;
   email: string;
+  vehicle: string;
+  timeIn: Timestamp | null;
 };
 
 const DataList: React.FC<{ tab: string }> = ({ tab }) => {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      setLoading(true);
-      const querySnapshot = await getDocs(collection(firestore, 'users'));
-      const usersData: User[] = [];
-      querySnapshot.forEach((doc) => {
-        usersData.push({ id: doc.id, ...doc.data() } as User);
-      });
-      setUsers(usersData);
-      setLoading(false);
-    };
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
+    const querySnapshot = await getDocs(collection(firestore, 'users'));
+    const usersData: User[] = querySnapshot.docs.map(doc => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        firstName: data.firstName,
+        email: data.email,
+        vehicle: data.vehicle,
+        timeIn: data.timeIn instanceof Timestamp ? data.timeIn : null
+      } as User;
+    });
+    setUsers(usersData);
+    setLoading(false);
+  }, []);
 
+  useEffect(() => {
     fetchUsers();
-  }, [tab]);
+  }, [fetchUsers, tab]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -35,8 +43,8 @@ const DataList: React.FC<{ tab: string }> = ({ tab }) => {
     <div className="flex flex-col space-y-4">
       {users.map(user => (
         <div key={user.id} className="p-4 bg-primary rounded-lg shadow">
-          {tab === 'logs' && <p>{user.email}</p>}
-          {tab === 'vehicles' && <p>{user.firstName}</p>}
+          {tab === 'logs' && <p>{user.timeIn ? user.timeIn.toDate().toLocaleString() : 'No time available'}</p>}
+          {tab === 'vehicles' && <p>{user.vehicle}</p>}
         </div>
       ))}
     </div>
