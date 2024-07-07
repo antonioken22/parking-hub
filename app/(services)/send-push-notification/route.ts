579dcae1b981c5895412091880ed9e8bab8503a6
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-07-08 06:17:31 +0800

    [Fix][Antonio] Fixed the "The default Firebase app does not exist. " error

--- app/(services)/send-push-notification/route.ts
+++ app/(services)/send-push-notification/route.ts
@@ -1,33 +1,22 @@
-import admin from "firebase-admin";
-import { Message } from "firebase-admin/messaging";
+import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
 import { NextRequest, NextResponse } from "next/server";
 
 // Initialize Firebase Admin SDK
-const serviceAccount = {
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
-
-// Initialize Firebase Admin SDK only if all required variables are defined
-if (!admin.apps.length && Object.values(serviceAccount).every(Boolean)) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
   });
-} else {
-  console.error("Missing Firebase credentials in environment variables");
 }
 
 export async function POST(request: NextRequest) {
   const { token, title, message, link } = await request.json();
 
-  const payload: Message = {
+  const payload = {
     token,
     notification: {
       title: title,
@@ -41,7 +30,7 @@ export async function POST(request: NextRequest) {
   };
 
   try {
-    await admin.messaging().send(payload);
+    await getMessaging().send(payload);
 
     return NextResponse.json({ success: true, message: "Notification sent!" });
   } catch (error) {
