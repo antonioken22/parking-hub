ecdd6dc15d1c35111297db66995056fc7de1903e
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-07-23 19:45:23 +0800

    [Adjustment][Antonio] Chat active users now polls every 1 minute

--- app/(routes)/_components/navbar.tsx
+++ app/(routes)/_components/navbar.tsx
@@ -9,12 +9,12 @@ import NotificationDropdown from "./notification-dropdown";
 import UserProfilePhoto from "./user-profile-photo";
 import useUserState from "@/hooks/useUserState";
 import useActiveUsers from "@/hooks/useActiveUsers";
-
-useActiveUsers;
+import useChatActiveUsers from "@/hooks/useChatActiveUsers";
 
 const Navbar = () => {
   const { userId, userEmail, userFirstName, userLastName } = useUserState();
   const { logActiveUser } = useActiveUsers();
+  const { logChatActiveUser } = useChatActiveUsers();
 
   // Logs the active user when he visits any part of the app
   // since the the navbar is visible and called on all parts of
@@ -22,8 +22,38 @@ const Navbar = () => {
   useEffect(() => {
     if (userId && userEmail && userFirstName && userLastName) {
       logActiveUser(userId, userEmail, userFirstName, userLastName);
+      logChatActiveUser(userId, userEmail, userFirstName, userLastName);
+
+      // Gets device current current start of the minute (ex. 7h:40m:00s)
+      const now = new Date();
+      const millisecondsUntilNextMinute =
+        (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
+
+      const startInterval = () => {
+        logChatActiveUser(userId, userEmail, userFirstName, userLastName);
+
+        // Then polls every 1 minute from there
+        const interval = setInterval(() => {
+          logChatActiveUser(userId, userEmail, userFirstName, userLastName);
+        }, 60000);
+
+        return () => clearInterval(interval);
+      };
+
+      const timeout = setTimeout(() => {
+        startInterval();
+      }, millisecondsUntilNextMinute);
+
+      return () => clearTimeout(timeout);
     }
-  }, [userId, userEmail, userFirstName, userLastName, logActiveUser]);
+  }, [
+    userId,
+    userEmail,
+    userFirstName,
+    userLastName,
+    logActiveUser,
+    logChatActiveUser,
+  ]);
 
   return (
     <div className="flex items-center p-4">
