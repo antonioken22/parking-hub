bf3cb535caec610bac29c4dcf80ffac143a932db
Kenette John A. Antonio <114849172+antonioken22@users.noreply.github.com>
2024-07-26 12:42:45 +0800

    Update config.ts

--- firebase/config.ts
+++ firebase/config.ts
@@ -5,13 +5,13 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD8_dkUAnbZQWK72eU87mVp_mo9DyU4Aew",
+  authDomain: "parking-hub-presentation.firebaseapp.com",
+  projectId: "parking-hub-presentation",
+  storageBucket: "parking-hub-presentation.appspot.com",
+  messagingSenderId: "372530545580",
+  appId: "1:372530545580:web:d7cddb183cb1e06137f009",
+  measurementId: "G-92695FGRDC"
 };
 
 // Spinning up Firebase
