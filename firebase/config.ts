dc2cfc6b1243ad3c908f3f18fa3bbbd90dd1baf0
Noturius247 <luzaresbenzgerald@gmail.com>
2024-11-08 19:01:30 +0800

    added new firebase

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,51 +1,23 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getMessaging, getToken, isSupported } from "firebase/messaging";
-import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";//new 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,//new
 };
 
-// Spinning up Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// Firebase Authentication
 const auth = getAuth(app);
+const firestore = getFirestore(app); // This should be 
+const database = getDatabase(app);
 
-// Firebase Firestore
-const firestore = getFirestore(app);
-
-// Firebase Messaging
-const messaging = async () => {
-  const supported = await isSupported();
-  return supported ? getMessaging(app) : null;
-};
-
-export const fetchToken = async () => {
-  try {
-    const fcmMessaging = await messaging();
-    if (fcmMessaging) {
-      const token = await getToken(fcmMessaging, {
-        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_FCM_VAPID_KEY,
-      });
-      return token;
-    }
-    return null;
-  } catch (err) {
-    console.error("An error occurred while fetching the token:", err);
-    return null;
-  }
-};
-
-//Firebase Storage
-const storage = getStorage(app);
-
-export { app, auth, firestore, storage, messaging };
+export { app, auth, firestore, database }; //new
